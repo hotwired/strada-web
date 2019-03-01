5cdeff0972e9fd1d49a802fc9ea712bea75e6e7f
Zach Waugh <zwaugh@gmail.com>
2019-03-01 11:48:29 -0500

    Add inital iOS framework

---
+++ ios/Source/Message.swift
@@ -0,0 +1,65 @@
+//
+//  Message.swift
+//  Strata
+//
+//  Created by Zach Waugh on 3/1/19.
+//  Copyright © 2019 Basecamp. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+public typealias MessageData = [String: Any]
+
+public struct Message {
+    /// A unique identifier for this message. You can reply to messages by sending
+    /// the same message back, or creating a new message with the same id
+    public let id: String
+    
+    /// The component the message is sent from (e.g. - "form", "page", etc)
+    public let component: String
+    
+    /// The event that this message is about: "submit", "display", "send"
+    public let event: String
+    
+    /// Any data to send along with the message, for a "page" component, this might be the ["title": "Page Title"]
+    public let data: MessageData
+    
+    public init(id: String, component: String, event: String, data: MessageData) {
+        self.id = id
+        self.component = component
+        self.event = event
+        self.data = data
+    }
+    
+    func toJSON() -> [String: Any] {
+        return ["id": id, "component": component, "event": event, "data": data]
+    }
+    
+    func replacing(data updatedData: [String: Any]) -> Message {
+        return Message(id: id, component: component, event: event, data: updatedData)
+    }
+    
+    func merging(data updatedData: [String: Any]) -> Message {
+        var mergedData = data
+        updatedData.forEach { mergedData[$0] = $1 }
+        
+        return Message(id: id, component: component, event: event, data: mergedData)
+    }
+}
+
+extension Message {
+    init?(scriptMessage: WKScriptMessage) {
+        guard let message = scriptMessage.body as? [String: Any],
+            let id = message["id"] as? String,
+            let component = message["component"] as? String,
+            let event = message["event"] as? String,
+            let data = message["data"] as? MessageData else {
+                print("[bridge-event] *** error parsing script message: \(scriptMessage.body)")
+                return nil
+        }
+        
+        self.init(id: id, component: component, event: event, data: data)
+    }
+}
+
---
+++ ios/Source/strata.js
@@ -0,0 +1,62 @@
+(() => {
+  // This represents the adapter that is installed on the WebBridge
+  // All adapters implement the same interface so the web doesn't need to
+  // know anything specific about the client platform
+  class NativeBridge {
+    constructor() {
+      this.supportedComponents = []
+    }
+
+    register(component) {
+      if (Array.isArray(component)) {
+        this.supportedComponents = this.supportedComponents.concat(component)
+      } else {
+        this.supportedComponents.push(component)
+      }
+
+      this.notifyBridgeOfSupportedEventsUpdate()
+    }
+
+    unregister(component) {
+      const index = this.supportedComponents.indexOf(component)
+      if (index != -1) {
+        this.supportedComponents.splice(index, 1)
+        this.notifyBridgeOfSupportedComponentsUpdate()
+      }
+    }
+
+    notifyBridgeOfSupportedComponentsUpdate() {
+      Bridge.adapterDidUpdateSupportedComponents()
+    }
+
+    supportsComponent(component) {
+      return this.supportedComponents.includes(component)
+    }
+
+    // Send message to web
+    send(message) {
+      window.WebBridge.receive(message)
+    }
+
+    // Receive from web
+    receive(message) {
+      this.postMessage(message)
+    }
+
+    get platform() {
+      return "ios"
+    }
+
+    // Native handler
+
+    postMessage(message) {
+      webkit.messageHandlers.strata.postMessage(message)
+    }
+  }
+
+  window.NativeBridge = new NativeBridge()
+
+  document.addEventListener("web-bridge:ready", () => {
+    window.WebBridge.setAdapter(appBridge)
+  })
+})()
