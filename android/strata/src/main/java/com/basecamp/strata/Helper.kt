113dc3b2eeafbcaf64b80ce01cd94eef8d8c4621
Jay Ohms <jay.ohms@gmail.com>
2019-04-03 11:48:41 -0500

    Tweak runOnUIThread and add clarifying comment

--- android/strata/src/main/java/com/basecamp/strata/Helper.kt
+++ android/strata/src/main/java/com/basecamp/strata/Helper.kt
@@ -4,13 +4,15 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 
+/**
+ * Guarantees main thread execution, posting a Runnable on
+ * the main Looper if necessary. This allows compatibility
+ * with unit tests that are already on the main thread.
+ */
 internal fun runOnUiThread(func: () -> Unit) {
-    if (Looper.myLooper() != Looper.getMainLooper()) {
-        Handler(Looper.getMainLooper()).post { func() }
-    } else {
-        // Avoid posting a runnable if it's not necessary,
-        // which also creates an easier ability to unit test.
-        func()
+    when (val mainLooper = Looper.getMainLooper()) {
+        Looper.myLooper() -> func()
+        else -> Handler(mainLooper).post { func() }
     }
 }
 
